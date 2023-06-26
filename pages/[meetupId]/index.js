5cc18dc811426cc645283135ddc1d4423adc8c53
axhar-ibraheem <azharibraheem482@gmail.com>
2023-06-26 12:31:52 +0530

    prerendering

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,16 +1,54 @@
-const MeetupDetails = () => {
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+import { useRouter } from "next/router";
+const MeetupDetail = () => {
   return (
-    <>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-        alt=""
-        style={{ width: "100%" }}
-      />
-      <h1>Meetup Title</h1>
-      <address>Meetup Adress</address>
-      <p>Meetup description</p>
-    </>
+    <MeetupDetails
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      title="Meetup Title"
+      address="Meetup Address"
+      description="Meetup description"
+    />
   );
 };
 
-export default MeetupDetails;
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+      {
+        params: {
+          meetupId: "m3",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetUpData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: "m1",
+        title: "Meetup Title",
+        address: "Meetup Address",
+        description: "Meetup description",
+      },
+    },
+  };
+}
+
+export default MeetupDetail;
--- pages/index.js
+++ pages/index.js
@@ -17,14 +17,6 @@ const dummyMeetups = [
     address: "some address, 5, some city",
     description: "This is a second meetup",
   },
-  {
-    id: "m3",
-    title: "A first meetup",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
-    address: "some address, 5, some city",
-    description: "This is a third meetup",
-  },
 ];
 
 const HomePage = () => {
